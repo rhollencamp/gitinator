5bfd5999f9a448f802e7f53f33f61fdf514f5e2a
Robert Hollencamp <rhollencamp@gmail.com>
2026-03-13 16:41:47 -0400

    Home Page
    derived from bootstrap template

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from gitinator import views
+
 urlpatterns = [
+    path("", views.home, name="home"),
     path("admin/", admin.site.urls),
 ]
