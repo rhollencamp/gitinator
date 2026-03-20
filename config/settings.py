768dd9829670c9435a37aba6201096a862f51b73
Robert Hollencamp <rhollencamp@gmail.com>
2026-03-20 00:46:55 -0400

    prod security settings (#11)

--- config/settings.py
+++ config/settings.py
@@ -27,9 +27,16 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
-
-ALLOWED_HOSTS = [h for h in os.environ.get("ALLOWED_HOSTS", "").split(",") if h]
-
+if not DEBUG:
+    ALLOWED_HOSTS = [h for h in os.environ.get("ALLOWED_HOSTS", "").split(",") if h]
+    CSRF_TRUSTED_ORIGINS = [
+        "https://" + h for h in os.environ.get("ALLOWED_HOSTS", "").split(",") if h
+    ]
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+else:
+    ALLOWED_HOSTS = []
 
 # Application definition
 
